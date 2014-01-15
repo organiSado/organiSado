2c38afeeaec64cfb1f8b6206a786e8fe8e9785a3
Joel Quatrocchi <joel.quatro@gmail.com>
2014-01-15 01:16:00 -0300

    capturar var dump a un string en update.php

--- tools/update.php
+++ tools/update.php
@@ -3,7 +3,10 @@
 $emails = array(/*"mjheredia88@gmail.com", "martinmatus100@gmail.com", "leonardo_celedon@hotmail.com",*/ "joel.quatro@gmail.com");
 $subject = "Nuevo commit en organiSado!";
 //$message = implode(", <br>\n\n", $_POST);
-$message = var_dump($_POST);
+ 
+ob_start();
+var_dump($_POST);
+$message = ob_get_clean();
 
 foreach ($emails as $email)
 {
@@ -11,4 +14,3 @@ foreach ($emails as $email)
 }
 
 ?>
-
