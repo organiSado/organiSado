067e85472b4cb11d7c1fcd9662a4587d37894ebd
Joel Quatrocchi <joel.quatro@gmail.com>
2014-01-30 03:15:16 -0300

    Query SQL "Mis Eventos"
    Cambio de nombre de alias.

--- base de datos/sql queries/mis_eventos.sql
+++ base de datos/sql queries/mis_eventos.sql
@@ -1,27 +1,40 @@
-SELECT	events.name,
-		events.location,
-		events.date,
-		events.admin,
+SELECT	datos_eventos_invitado_con_cant_invitados.name,
+		datos_eventos_invitado_con_cant_invitados.location,
+		datos_eventos_invitado_con_cant_invitados.date,
+		datos_eventos_invitado_con_cant_invitados.admin,
 		confirmados.cant_confirmados,
-		events.cant_invitados
+		datos_eventos_invitado_con_cant_invitados.cant_invitados
 FROM
-	(SELECT events.name, events.location, events.date, events.admin, invitees.cant_invitados, events.id
-	FROM
-		(SELECT * FROM
+    (SELECT datos_eventos_invitado.name,
+            datos_eventos_invitado.location,
+            datos_eventos_invitado.date,
+            datos_eventos_invitado.admin,
+            invitados_por_evento.cant_invitados,
+            datos_eventos_invitado.id
+    FROM
+        (SELECT *
+		FROM
 			organisado.events
-		RIGHT JOIN
-			(SELECT * FROM organisado.invitees WHERE email='email2') AS eventos_invitado
+		RIGHT JOIN 
+			(SELECT *
+			FROM
+				organisado.invitees
+			WHERE
+				email = 'email2') AS eventos_invitado
 		ON
-			events.id=eventos_invitado.event) AS events
-	INNER JOIN 
-		(SELECT COUNT(*) AS cant_invitados, event FROM
-			organisado.invitees GROUP BY event) AS invitees
-	ON
-		(events.id=invitees.event) ) AS events
-INNER JOIN 
+			events.id = eventos_invitado.event) AS datos_eventos_invitado
+    INNER JOIN
+		(SELECT 
+			COUNT(*) AS cant_invitados,
+			event
+		FROM
+			organisado.invitees
+		GROUP BY event) AS invitados_por_evento
+	ON (datos_eventos_invitado.id = invitados_por_evento.event)) AS datos_eventos_invitado_con_cant_invitados
+INNER JOIN
 	(SELECT 
-		COUNT(CASE confirmed WHEN 1 THEN 1 ELSE NULL END) AS cant_confirmados, event
-	FROM
-		organisado.invitees GROUP BY event) AS confirmados
-ON
-	events.id=confirmados.event;
+		COUNT(CASE confirmed WHEN 1 THEN 1 ELSE NULL END) AS cant_confirmados,
+		event
+    FROM
+        organisado.invitees GROUP BY event) AS confirmados
+ON datos_eventos_invitado_con_cant_invitados.id = confirmados.event;
