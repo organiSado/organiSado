2bf22e59fcb341a3ee932fb24bf98149e50982b0
Joel Quatrocchi <joel.quatro@gmail.com>
2014-01-29 22:52:59 -0300

    Terminos
    sacado <hr> excedente.

--- organisado.com.ar/protected/views/site/terminos.php
+++ organisado.com.ar/protected/views/site/terminos.php
@@ -40,5 +40,3 @@ $this->breadcrumbs=array(
         <p class="lead ">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
     </div>
 </div>
-
-<hr>
