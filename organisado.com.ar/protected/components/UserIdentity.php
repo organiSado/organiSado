a4e2f807b141ef00327f77d5b7da3caa229a9e15
martinmatus100 <martinmatus100@gmail.com>
2014-02-05 23:50:04 -0300

    Conexión con la BBDD y prueba de LOGIN
    Se realizó la conexión con la base de datos para poder loguearse con
    usuario valido.  Como los usuarios que estan cargados en la BBDD tienen
    como atributos Email (tiene que tener un @, lo cual la validacion no lo
    permite) y contraseña(hay que encriptarla), a modo de prueba, ingresen
    con el usuario= yii    y    contraseña=yii

--- organisado.com.ar/protected/components/UserIdentity.php
+++ organisado.com.ar/protected/components/UserIdentity.php
@@ -5,8 +5,7 @@
  * It contains the authentication method that checks if the provided
  * data can identity the user.
  */
-class UserIdentity extends CUserIdentity
-{
+class UserIdentity extends CUserIdentity {
 	/**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
@@ -15,19 +14,36 @@ class UserIdentity extends CUserIdentity
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
-	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
+	public function authenticate() {
+		/*
+		 $users=array(
+		 // username => password
+		 'demo'=>'demo',
+		 'admin'=>'admin',
+		 );
+		 if(!isset($users[$this->username]))
+		 $this->errorCode=self::ERROR_USERNAME_INVALID;
+		 elseif($users[$this->username]!==$this->password)
+		 $this->errorCode=self::ERROR_PASSWORD_INVALID;
+		 else
+		 $this->errorCode=self::ERROR_NONE;
+		 return !$this->errorCode;
+		*/ 
+		 
+		$atts = array('email' => $this -> username, //traspasa el "nombre" mandado por formulario (models / LoginForm.php)
 		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
+
+		$usuario = Users::model() -> findByAttributes($atts);
+		//busca este nombre en la bdd usando el USER.PHP creado con el CRUD que esta en la carpeta Model
+
+		if (($usuario === null))
+			$this -> errorCode = self::ERROR_USERNAME_INVALID;
+		elseif ($usuario -> password_hash !== $this -> password)
+			$this -> errorCode = self::ERROR_PASSWORD_INVALID;
 		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
+			$this -> errorCode = self::ERROR_NONE;
+		return !$this -> errorCode;
+
 	}
+
 }
--- organisado.com.ar/protected/config/main.php
+++ organisado.com.ar/protected/config/main.php
@@ -62,20 +62,20 @@ return array(
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
-		),
+		),/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		),*/
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=db.organisado.com.ar;dbname=organisado',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'yiidborganisado',
+			'password' => 'organisad0',
 			'charset' => 'utf8',
 		),
-		*/
+		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
