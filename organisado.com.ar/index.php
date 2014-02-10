147ce798aa4c5ea2bb01f9e6ee0dda039515f5af
Joel Quatrocchi <joel.quatro@gmail.com>
2014-02-10 17:07:33 -0300

    Debugging OFF
    Sacado Debugging de Yii del server de producción,
    y agregado cartel de modo localhost(ip) para diferenciar la página
    local de la online.

--- organisado.com.ar/index.php
+++ organisado.com.ar/index.php
@@ -4,10 +4,15 @@
 $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+// Debug solo en localhost
+if ($_SERVER['SERVER_ADDR'] == "::1")
+{
+	echo "<div style='position:fixed;top:0;left:0;padding:0 2px;color:white;background:red;z-index:99999;'>MODE: localhost (".$_SERVER['SERVER_ADDR'].")</div>";
+	// remove the following lines when in production mode
+	defined('YII_DEBUG') or define('YII_DEBUG',true);
+	// specify how many levels of call stack should be shown in each log message
+	defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);	
+}
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
