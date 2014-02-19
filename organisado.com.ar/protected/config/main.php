a4db5011694ecfbe9ac57669a61cf4f12694e2e2
Joel Quatrocchi <joel.quatro@gmail.com>
2014-02-19 18:16:32 -0300

    Url Manager Desactivado temporalmente
    desactivado el url manager se "arreglaron" varios problemas
    
    - delete/update/view de usuarios
    - error que vio mauro strione cuando te registras. La vista de perfil
    del usuario no era accesible porque faltaban reglas
    
    IMPORTATE: la idea es reactivarlo e ir creando todas las reglas
    necesarias para que no ocurran estos problemas.

--- organisado.com.ar/protected/config/main.php
+++ organisado.com.ar/protected/config/main.php
@@ -54,7 +54,7 @@ return array(
 		),
 		// uncomment the following to enable URLs in path-format
 		
-		'urlManager'=>array(
+		/*'urlManager'=>array(
 			'showScriptName'=>false,
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -62,7 +62,9 @@ return array(
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
-		),/*
+		),*/
+
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),*/
