0b2601fbd08ea3a2ccd112642d76971945f301cd
leonardoceledon <leoce911@gmail.com>
2014-05-15 03:43:15 -0300

    Restricción para los usuarios
    No permite que un usuario no registrado vea las publicaciones

--- organisado.com.ar/protected/controllers/EventsController.php
+++ organisado.com.ar/protected/controllers/EventsController.php
@@ -27,12 +27,12 @@ class EventsController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
+			/*array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array(),
 				'users'=>array('*'),
-			),
+			),*/
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','delete'),
+				'actions'=>array('index','view','create','update','delete'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
