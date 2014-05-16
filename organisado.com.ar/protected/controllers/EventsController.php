cfd2e4c55046bbc8f24efe7d6c95c52bc1380e09
Joel Quatrocchi <joel.quatro@gmail.com>
2014-05-16 01:33:18 -0300

    Update Tabular terminado
    codigo estable para update tabular

--- organisado.com.ar/protected/controllers/EventsController.php
+++ organisado.com.ar/protected/controllers/EventsController.php
@@ -61,136 +61,6 @@ class EventsController extends Controller
 		));
 	}
 	
-	
-	
-	
-	
-	
-	
-	/**
-	 * Returns the data model based on the primary key given in the GET variable.
-	 * If the data model is not found, an HTTP exception will be raised.
-	 * @param integer $id the ID of the model to be loaded
-	 * @return Events the loaded model
-	 * @throws CHttpException
-	 */
-	public function loadInviteesModels($id)
-	{
-		$table = Invitees::model()->tableName();
-		$models = Invitees::model()->findAllBySql("SELECT * FROM $table WHERE event=$id;");
-		if($models===null)
-			throw new CHttpException(404,'The requested page does not exist.');
-		
-		return $models;
-	}
-	
-	public function getItemsToUpdate($id) {
-        // Create an empty list of records
-        $items = array();
- 
-        // Iterate over each item from the submitted form
-        if (isset($_POST['Invitees']) && is_array($_POST['Invitees'])) {
-            foreach ($_POST['Invitees'] as $item) {
-                // If item id is available, read the record from database 
-                if ( array_key_exists('email', $item) && $id/* $id  *//* array_key_exists('id', $item) */ ){
-                    //$items[] = Invitees::model()->findByPk($item['id']);
-                    //$items[] = Invitees::model()->findByPk(array($item['email'], $id));
-                    //$items[] = $this->loadInviteesModel(/* $id */"23");
-/*$table = Invitees::model()->tableName();
-		$items[] = Invitees::model()->findAllBySql("SELECT * FROM $table WHERE event=$id;");
-		*/
-					$table = Invitees::model()->tableName();
-					$items[] = Invitees::model()->findBySql("SELECT * FROM $table WHERE email='".$item['email']."' AND event=$id;");
-		
-                }
-                // Otherwise create a new record
-                else {
-                echo "NEW";return;
-                    $items[] = new Invitees;
-                }
-            }
-        }
-        return $items;
-    }
-
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
-
-		// Cargamos modelo de invitados
-		$inviteesModels=$this->loadInviteesModels($id);
-		if (!count($inviteesModels))
-			array_push($inviteesModels, new Invitees);
-			
-		// Uncomment the following line if AJAX validation is needed
-		$this->performAjaxValidation(array/*_merge*/($model, $inviteesModels));
-
-/* var_dump($_POST['Invitees']);return ; */
-
-$items=$this->getItemsToUpdate($id);
-
-		if(isset($_POST['Events'], $_POST['Invitees']))
-		{
-			$model->attributes=$_POST['Events'];
-			//$inviteesModels->attributes=$_POST['Invitees'];
-
-
-//var_dump($items);return ;
-/*
-			echo '$items';
-	    var_dump($items);
-*/
-$valid=true;
-foreach($items as $i=>$item)
-{
-
-    if(isset($_POST['Invitees'][$i]))
-    {
-        $item->attributes=$_POST['Invitees'][$i];
-    }
-    
-    $valid=$item->validate() && $valid;
-}
-
-if($valid)
-{
-/* $items->save(); */
-   foreach($items as $i=>$item)
-    {
-    	$item->save();
-    }
-}
-var_dump($items);return ;
-
-			// validate BOTH $a and $b
-            if($model->validate() && $inviteesModels->validate())
-			{
-				$model->save(false);
-				$inviteesModels->save(false);
-
-				$this->redirect(array('view','id'=>$model->id));
-			}
-		}
-
-		$this->render('update',array(
-			'model'=>$model,
-			'inviteesModels'=>$inviteesModels
-		));
-	}
-	
-	
-	
-	
-	
-	
-
-
-
-
-
-
-
-
 	/**
 	 * Creates a new model.
 	 * If creation is successful, the browser will be redirected to the 'view' page.
@@ -234,26 +104,47 @@ var_dump($items);return ;
 	 * If update is successful, the browser will be redirected to the 'view' page.
 	 * @param integer $id the ID of the model to be updated
 	 */
-	/*public function actionUpdate($id)
+	public function actionUpdate($id)
 	{
 		$model=$this->loadModel($id);
 
 		// Cargamos modelo de invitados
-		$inviteesModel=$this->loadInviteesModel($id);
-
+		$inviteesModels=$this->loadInviteesModels($id);
+		if (!count($inviteesModels))
+			array_push($inviteesModels, new Invitees);
+			
 		// Uncomment the following line if AJAX validation is needed
-		$this->performAjaxValidation(array($model, $inviteesModel));
-
+		$this->performAjaxValidation(array/*_merge*/($model, $inviteesModels));
+		
 		if(isset($_POST['Events'], $_POST['Invitees']))
 		{
+			// add changes to event model
 			$model->attributes=$_POST['Events'];
-			$inviteesModel->attributes=$_POST['Invitees'];
 
-			// validate BOTH $a and $b
-            if($model->validate() && $inviteesModel->validate())
+			// add changes to invitees models
+			$validInvitees = true;
+			foreach($inviteesModels as $i=>$inviteesModel)
 			{
+			
+			    if(isset($_POST['Invitees'][$i]))
+			    {
+			    	$inviteesModel->attributes = $_POST['Invitees'][$i];
+			    }
+			    
+			    $validInvitees = $inviteesModel->validate() && $validInvitees;
+			}
+			
+			// save changes
+            if($model->validate() && $validInvitees)
+			{
+				// to event model
 				$model->save(false);
-				$inviteesModel->save(false);
+				
+				// to invitees models
+				foreach($inviteesModels as $inviteesModel)
+				{
+					$inviteesModel->save();
+				}
 
 				$this->redirect(array('view','id'=>$model->id));
 			}
@@ -261,9 +152,9 @@ var_dump($items);return ;
 
 		$this->render('update',array(
 			'model'=>$model,
-			'inviteesModel'=>$inviteesModel
+			'inviteesModels'=>$inviteesModels
 		));
-	}*/
+	}
 
 	/**
 	 * Deletes a particular model.
@@ -343,14 +234,15 @@ var_dump($items);return ;
 	 * @return Events the loaded model
 	 * @throws CHttpException
 	 */
-/*	public function loadInviteesModel($id)
+	public function loadInviteesModels($id)
 	{
 		$table = Invitees::model()->tableName();
-		$model = Invitees::model()->findBySql("SELECT * FROM $table WHERE event=$id;");
-		if($model===null)
+		$models = Invitees::model()->findAllBySql("SELECT * FROM $table WHERE event=$id;");
+		if($models===null)
 			throw new CHttpException(404,'The requested page does not exist.');
-		return $model;
-	}*/
+		
+		return $models;
+	}
 
 	/**
 	 * Performs the AJAX validation.
