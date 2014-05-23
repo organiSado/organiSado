9749dfc859edf40e95656919aa147ac45041ee8f
Mjeheredia <mjheredia88@gmail.com>
2014-05-23 08:47:59 -0300

    Cambio de lugar del yii chat
    movi la interfaz del yii chat al view de eventos

--- organisado.com.ar/protected/views/events/_form.php
+++ organisado.com.ar/protected/views/events/_form.php
@@ -102,24 +102,6 @@
 	<hr>
 
 
-	<h1>Muro de Mensajes</h1>
-<div id='chat'></div>
-<?php 
-    $this->widget('YiiChatWidget',array(
-        'chat_id'=>'123',                   // a chat identificator
-        'identity'=>1,                      // the user, Yii::app()->user->id ?
-        'selector'=>'#chat',                // were it will be inserted
-        'minPostLen'=>2,                    // min and
-        'maxPostLen'=>80,                   // max string size for post
-        'model'=>new ChatHandler(),    // the class handler. **** FOR DEMO, READ MORE LATER IN THIS DOC ****
-        'data'=>'any data',                 // data passed to the handler
-        // success and error handlers, both optionals.
-        'onSuccess'=>new CJavaScriptExpression(
-            "function(code, text, post_id){   }"),
-        'onError'=>new CJavaScriptExpression(
-            "function(errorcode, info){  }"),
-    ));
-?>
 
 
 
